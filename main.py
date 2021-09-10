740b0c5e3c3f700ac0e57b1c2975344199225c4a
Ramón Vásquez <ramvas1984@gmail.com>
2021-09-10 19:05:33 -0300

    Renamed controller as main - Added documentation

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+This program is a basic scraper for IMDb's Top 250. It is fully built
+on Tkinter.
+
+When loading, the program scrapes the top 250 film list and shows some
+basic info for each film when selected. This data is temporarily saved
+into a MySQL table.
+
+Double-clicking on any film will open a second window that shows more
+detailed information about the movie, i.e., directors, writers, full
+cast, some technical data, poster, and social media links. This data
+is saved into a dictionary, instead of a database.
+"""
+from tkinter import Tk
+
+from view import MainWindow
+
+
+class IMDbScraper:
+    def __init__(self, root):
+        self.root = root
+        MainWindow(self.root)
+
+
+if __name__ == "__main__":
+    root = Tk()
+    app = IMDbScraper(root)
+    root.mainloop()
